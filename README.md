9a53a237ada1df411e20b49c04c77aaf44a03a95
JohnCoene <jcoenep@gmail.com>
2023-11-10 18:33:49 +0100

    fix: retrieve first visible child

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<!-- badges: start -->
+!-- badges: start -->
 <!-- badges: end -->
 
 # masonry
--- srcjs/config.js
+++ srcjs/config.js
@@ -2,32 +2,49 @@ import { getGrid } from "./storage.js";
 
 export const getConfig = (opts) => {
   let rows = [];
-  $(`#${opts.target}`).find(".masonry-row").each((i, row) => {
-    let items = [];
+  $(`#${opts.target}`)
+    .find(".masonry-row")
+    .each((i, row) => {
+      let items = [];
 
-    $(row).find(".masonry-item").each((i, item) => {
-      let opt = {
-        width: getDimensions(item),
-        id: $(item).attr("id"),
+      $(row)
+        .find(".masonry-item")
+        .each((i, item) => {
+          const first = [];
+
+          $(item)
+            .children()
+            .each((_i, el) => {
+              if (!$(el).is(":visible")) return;
+              first.push(el);
+            });
+
+          let childId = "";
+          if (first.length > 0) childId = $(first[0]).attr("id");
+
+          let opt = {
+            width: getDimensions(item),
+            id: $(item).attr("id"),
+            childId: childId,
+          };
+
+          items.push(opt);
+        });
+
+      let total = items.map((item) => item.width).reduce((c, p) => c + p);
+
+      items = items.map((item) => {
+        item.percentage = asPercentage(item.width, total);
+        return item;
+      });
+
+      let r = {
+        id: $(row).attr("id"),
+        items: items,
       };
 
-      items.push(opt);
+      rows.push(r);
     });
-
-    let total = items.map((item) => item.width).reduce((c, p) => c + p);
-
-    items = items.map((item) => {
-      item.percentage = asPercentage(item.width, total);
-      return item;
-    });
-
-    let r = {
-      id: $(row).attr("id"),
-      items: items,
-    };
-
-    rows.push(r);
-  });
 
   let config = {
     opts: getGrid(`#${opts.target}`),
@@ -49,27 +66,31 @@ export const restoreConfig = (opts) => {
   rearrangeGrid(opts);
 
   // resize widths
-  $(`#${opts.target}`).find(".masonry-row").each((ri, row) => {
-    $(row).find(".masonry-item").each((ii, item) => {
-      $(item).css("width", `${opts.config.grid[ri].items[ii].percentage}%`);
-      $(item).trigger("resize");
+  $(`#${opts.target}`)
+    .find(".masonry-row")
+    .each((ri, row) => {
+      $(row)
+        .find(".masonry-item")
+        .each((ii, item) => {
+          $(item).css("width", `${opts.config.grid[ri].items[ii].percentage}%`);
+          $(item).trigger("resize");
+        });
     });
-  });
 };
 
 const rearrangeGrid = (opts) => {
   let items = [];
 
   opts.config.grid.map((row, rowIndex) => {
-    const existingRowId = $(`#${opts.target}`).find(
-      `.masonry-row:eq(${rowIndex})`,
-    ).attr("id");
+    const existingRowId = $(`#${opts.target}`)
+      .find(`.masonry-row:eq(${rowIndex})`)
+      .attr("id");
     const toRowId = row.id;
 
     row.items.map((item, itemIndex) => {
-      const existingItemId = $(`#${opts.target}`).find(
-        `.masonry-item:eq(${itemIndex})`,
-      ).attr("id");
+      const existingItemId = $(`#${opts.target}`)
+        .find(`.masonry-item:eq(${itemIndex})`)
+        .attr("id");
       const toItemId = item.id;
 
       $(`#${toItemId}`).appendTo(`#${toRowId}`);
